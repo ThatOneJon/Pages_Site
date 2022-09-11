b64984e23ee8895063926320a540529630b85512
mortal Wombat <83345498+ThatOneJon@users.noreply.github.com>
2022-09-12 01:20:57 +0200

    extending

--- scripts.js
+++ scripts.js
@@ -1,19 +1,32 @@
 document.addEventListener("DOMContentLoaded", ()=> {
+    
     let ProjectsButton = document.getElementById("btn_1");
     let About = document.getElementById("btn_2");
     let Contact = document.getElementById("btn_3");
 
     document.getElementById("view_1").style.display="block";
     document.getElementById("view_2").style.display="none";
+    document.getElementById("view_3").style.display="none";    
+
 
     ProjectsButton.onclick = () => {
         document.getElementById("view_1").style.display="block";
         document.getElementById("view_2").style.display="none";
+        document.getElementById("view_3").style.display="none";    
+
     };
 
     About.onclick= () => {
         document.getElementById("view_1").style.display="none";
         document.getElementById("view_2").style.display="block";
-    }
+        document.getElementById("view_3").style.display="none";    
 
+    };
+
+    Contact.onclick = () => {
+        document.getElementById("view_1").style.display="none";
+        document.getElementById("view_2").style.display="none";    
+        document.getElementById("view_3").style.display="block";    
+
+    };
 })
